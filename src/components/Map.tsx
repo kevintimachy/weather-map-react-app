071f8754ee3cdac765342c2a8c37c7d3eeb001c4
kevintimachy <your-kevintimachy@gmail.com>
2026-02-22 11:54:38 -0500

    fix(map): leaflet marker icons in production

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, Marker, useMap, useMapEvents } from 'react-leaflet'
+import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 import { useLocation } from '../context/LocationContext'
 import SearchBar from './SearchBar'
 import {
@@ -14,6 +18,13 @@ import {
 } from './ui/dropdown-menu'
 
 const API_KEY = import.meta.env.VITE_API_KEY
+
+delete (L.Icon.Default.prototype as { _getIconUrl?: unknown })._getIconUrl
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+})
 
 
 type WeatherLayer =
